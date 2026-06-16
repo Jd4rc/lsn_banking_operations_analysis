064776e04f5ab0923c1243d6ae9fdd91eb539f67
dosed <e.nmitenin@gmail.com>
2026-06-16 17:00:44 +0300

    add get cards info func

--- src/services.py
+++ src/services.py
@@ -1,5 +1,33 @@
-def get_cards_info():
-    ...
+def get_cards_info(
+        operations: list[dict],
+
+) -> list[dict]:
+    cards: dict[str, float] = {}
+
+    for operation in operations:
+        card_number = operation.get('Номер карты')
+        amount = operation.get('Сумма платежа', 0)
+
+        if not card_number:
+            continue
+
+        if amount >= 0:
+            continue
+
+        card_number = str(card_number)
+        last_digits = card_number[-4:]
+
+        cards[last_digits] = cards.get(last_digits, 0) + abs(amount)
+
+
+    return [
+        {
+            'last_digits': last_digits,
+            'total_spent': round(total_spent, 2),
+            'cashback': round(total_spent/100, 2)
+        }
+        for last_digits, total_spent in cards.items()
+    ]
 
 def get_top_transactions(operations: list[dict]) -> list[dict]:
     sorted_operations = sorted(
