5b879fa6bca32f323d08f8e71b46e0873158a6cc
dosed <e.nmitenin@gmail.com>
2026-06-16 16:10:37 +0300

    add func get_top_transactions

--- src/services.py
+++ src/services.py
@@ -1,8 +1,26 @@
 def get_cards_info():
     ...
 
-def get_top_transactions():
-    ...
+def get_top_transactions(operations: list[dict]) -> list[dict]:
+    sorted_operations = sorted(
+        operations,
+        key=lambda operation: abs(operation['Сумма платежа']),
+        reverse=True
+    )
+
+    top_operations = sorted_operations[:5]
+
+    return [
+        {
+            "date": operation['Дата операции'],
+            "amount": operation['Сумма платежа'],
+            "category": operation['Категория'],
+            "description": operation['Описание']
+        }
+        for operation in top_operations
+    ]
+
+
 
 def get_currency_rates():
     ...
