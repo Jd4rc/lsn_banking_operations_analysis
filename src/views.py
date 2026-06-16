3d61d98d33f0b274091a4657d1ab075034b77ab1
dosed <e.nmitenin@gmail.com>
2026-06-16 15:59:52 +0300

    update func

--- src/views.py
+++ src/views.py
@@ -3,8 +3,8 @@ from src.utils import get_greeting
 def main(date_time: str) -> dict:
     return {
         "greeting": get_greeting(date_time),
-        "cards": get_cards_info(),
-        "top_transactions": get_top_transactions(),
+        "cards": [],
+        "top_transactions": [],
         "currency_rates": [],
         "stock_prices": [],
     }
