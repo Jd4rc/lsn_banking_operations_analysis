7a284e128208b6a58a4ae4a4289d6da4e4609be4
dosed <e.nmitenin@gmail.com>
2026-06-18 21:14:03 +0300

    add get_currency_rates func

--- src/services.py
+++ src/services.py
@@ -1,3 +1,22 @@
+import os
+import json
+from pathlib import Path
+
+import requests
+from dotenv import load_dotenv
+from src.utils import load_user_settings
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+SETTING_PATH = BASE_DIR / 'user_setting.json'
+
+load_dotenv(BASE_DIR / '.env')
+
+settings = load_user_settings(SETTING_PATH)
+
+currencies = settings.get('user_currencies', [])
+stocks = settings.get('user_stocks', [])
+
+
 def get_cards_info(
         operations: list[dict],
 
@@ -50,8 +69,51 @@ def get_top_transactions(operations: list[dict]) -> list[dict]:
 
 
 
-def get_currency_rates():
-    ...
+def get_currency_rates(currencies: list[str]) -> list[dict]:
+    # 3. для каждой валюты сделать запрос к API
+    # 4. собрать список словарей
+    # 5. вернуть результат
+
+    api_key = os.getenv('API_KEY')
+
+    currency_rates = []
+
+    for currency in currencies:
+        url = "https://api.apilayer.com/exchangerates_data/convert"
+
+        params = {
+            'from': currency,
+            'to': 'RUB',
+            'amount': 1
+        }
+
+        headers = {
+            "apikey": api_key,
+        }
+
+        try:
+            response = requests.get(url, params=params, headers=headers, timeout = 10)
+
+            response.raise_for_status()
+
+            data = response.json()
+        except requests.RequestException:
+            continue
+
+        rate = data['result']
+
+        if rate is None:
+            continue
+
+
+        currency_rates.append(
+            {
+                'currency': currency,
+                'rate': round(rate, 2),
+            }
+        )
+
+    return currency_rates
 
 def get_stock_prices():
     ...
