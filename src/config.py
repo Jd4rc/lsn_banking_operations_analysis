2673ab37f564c106f6a5217edc36bc9e87cf5583
dosed <e.nmitenin@gmail.com>
2026-06-19 18:21:46 +0300

    move load_dotenv and api_key

--- src/config.py
+++ src/config.py
@@ -1,11 +1,17 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 DATA_DIR = BASE_DIR / 'data'
 
 SETTING_PATH = DATA_DIR / 'user_settings.json'
+
 OPERATIONS_PATH = DATA_DIR / 'user_operations.xlsx'
+
+load_dotenv(BASE_DIR / '.env')
+
+API_KEY = os.getenv('API_KEY')
 
 
