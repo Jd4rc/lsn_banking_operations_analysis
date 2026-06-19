715d896e2cd15e14e8cb3a9f285007b148976ddc
dosed <e.nmitenin@gmail.com>
2026-06-19 18:46:07 +0300

    update draft code for main()

--- src/views.py
+++ src/views.py
@@ -6,6 +6,7 @@ from src.utils import get_greeting
 from src.services import get_cards_info
 from src.services import get_top_transactions
 from src.services import get_currency_rates
+from src.services import get_stock_prices
 from src.utils import load_user_settings
 from src.utils import load_transactions
 
@@ -16,10 +17,12 @@ def main(date_time: str) -> dict:
 
     operations = load_transactions(OPERATIONS_PATH)
 
+    stocks = settings.get('user_stocks', [])
+
     return {
         "greeting": get_greeting(date_time),
         "cards": get_cards_info(operations),
         "top_transactions": get_top_transactions(operations),
         "currency_rates": get_currency_rates(currencies),
-        "stock_prices": [],
+        "stock_prices": get_stock_prices(stocks),
     }
