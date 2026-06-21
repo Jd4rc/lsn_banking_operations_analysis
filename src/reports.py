be628011ecddcefb3b5cff57f49a9f405c73a7ea
dosed <e.nmitenin@gmail.com>
2026-06-21 16:56:10 +0300

    add spending by category func

--- src/reports.py
+++ src/reports.py
@@ -4,7 +4,6 @@ from pathlib import Path
 from typing import Callable, Optional
 
 import pandas as pd
-from openpyxl.styles.builtins import output
 
 
 def report_to_file(filename: str | None = None,):
@@ -13,7 +12,6 @@ def report_to_file(filename: str | None = None,):
 
     Если имя файла не задано, создается файл с названиемм по умолчанию.
     """
-
     def decorator(func: Callable):
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -42,3 +40,42 @@ def report_to_file(filename: str | None = None,):
             return result
         return wrapper
     return decorator
+
+@report_to_file()
+def spending_by_category(
+        transactions: pd.DataFrame,
+        category: str,
+        date: Optional[str] = None
+):
+    """""
+    Возвращает траты по заданной категории за последниео три месяца.
+
+    :param transactions: DataFrame с транзакциями
+    :param category: Название категории.
+    :param date: Дата отсчета. Если не передеана, используется текущая дата.
+    :return: DataFrame с тратами по указанной категории.
+    """""
+
+    data = transactions.copy()
+
+    if date is None:
+        end_date = datetime.now()
+    else:
+        end_date = pd.to_datetime(date, errors='coerce')
+
+
+    start_date = end_date - pd.DateOffset(months=3)
+
+    data['Дата платежа'] = pd.to_datetime(
+        data['Дата платежа'],
+        errors='coerce',
+    )
+
+    result = data[
+        (data['Дата платежа'] >= start_date)
+        & (data['Дата платежа'] <= end_date)
+        & (data['Категория'] == category)
+        & (data['Сумма платежа'] < 0)
+    ]
+
+    return result
