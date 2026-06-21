b43d04218f0c14293d530f8749983c1b6fc03635
dosed <e.nmitenin@gmail.com>
2026-06-21 17:22:11 +0300

    add test for decorator report_to_file

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -28,3 +28,17 @@ def test_report_to_file_with_dataframe(tmp_path):
         {'Категория': 'Супермаркеты', 'Сумма платежа': -1000},
         {'Категория': 'Транспорт', 'Сумма платежа': -500},
     ]
+
+
+def test_report_to_file_with_text_result(tmp_path):
+    file_path = tmp_path / 'report.txt'
+
+    @report_to_file(str(file_path))
+    def test_func():
+        return 'Hello report'
+
+
+    result = test_func()
+    assert file_path.exists()
+    assert result == 'Hello report'
+    assert file_path.read_text(encoding='UTF-8') == 'Hello report'
